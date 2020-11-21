d180dec28ecd1052d653af5f2e556d2c9b5d5e58
unknown <haithong184@gmail.com>
2020-11-21 21:02:05 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Hello, world!123asdasdasdasdsfsdf
+            </div>
+        );
+    }
+}
